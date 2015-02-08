f18cd699f8f54ddd45e7cfa86baf8d4dba992ea9
Lunan <Simon@Kattler.dk>
2015-02-08 02:58:48 +0100

    Scene sammenligning
    Du kan nu gå fra scene1 til delivery til scene2

---
+++ Assets/scripts/bulletScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+
+	private GameObject player_Prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.gameObject.tag == "destroyable") {
+			//Debug.Log ("I hit something!");
+			Destroy (col.gameObject);
+			//Instantiate (player_Prefab, col.contacts [0].point, col.gameObject.transform.rotation);
+			Destroy (gameObject);
+		} else {
+			//Debug.Log ("I hit something! Too" + col.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/scripts/shootingScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootingScript : MonoBehaviour {
+
+
+	public Rigidbody2D bullet;
+	public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			//Debug.Log("pew?");
+			Rigidbody2D bulletClone = (Rigidbody2D)Instantiate(bullet, transform.position, transform.rotation);
+			bulletClone.gameObject.SetActive(true);
+			Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+			bulletClone.velocity = ((mousePos-gameObject.rigidbody2D.position)/(mousePos-gameObject.rigidbody2D.position).magnitude)*5;
+		}
+	}
+
+
+}
